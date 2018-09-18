39e4c65c453b67f651ab11eb40d5fe2075c655c2
JakubGutowski <jakubgutowski91+github@gmail.com>
2018-09-19 00:02:46 +0200

    Added first test for model and slightly refactored HTML templates

--- source/blog/tests.py
+++ source/blog/tests.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 from .models import BlogPost
 
 
+class BlogModelTestCase(TestCase):
+    def setUp(self):
+        BlogPost.objects.create(title="Post Number #1", text="lorem ipsum", )
 
-
-
-
+    def test_blog_post_created(self):
+        firstPost = BlogPost.objects.get(id=1)
+        self.assertEqual(firstPost.title, "Post Number #1")
--- source/blog/views.py
+++ source/blog/views.py
@@ -1,4 +1,3 @@
-from django.views.generic import ListView
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
