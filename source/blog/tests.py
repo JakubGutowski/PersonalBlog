a966c67f9e497d637285f4f728a4a6b5692dd146
JakubGutowski <jakubgutowski91+github@gmail.com>
2018-09-27 12:17:38 +0200

    added tests for ip loging

--- source/blog/tests.py
+++ source/blog/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
-from .models import BlogPost
+from django.test import TestCase, Client
+from django.shortcuts import render
+from .models import BlogPost, VisitorIp
+
 
 
 class BlogModelTestCase(TestCase):
@@ -8,4 +10,24 @@ class BlogModelTestCase(TestCase):
 
     def test_blog_post_created(self):
         firstPost = BlogPost.objects.get(id=1)
+        firstPost.save()
         self.assertEqual(firstPost.title, "Post Number #1")
+
+
+class MainPageTestCase(TestCase):
+    def setUp(self):
+        first = BlogPost.objects.create(title="Post Number #1", text="lorem ipsum", )
+        self.client = Client()
+        self.response = self.client.get('/')
+
+    def test_main_page_status(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_main_page_rendered_html(self):
+        posts_on_page = self.response.context['latest_posts']
+        self.assertEqual(posts_on_page[0], BlogPost.objects.get(id=1))
+
+    def test_main_page_ip_loging(self):
+        self.assertEqual(len(VisitorIp.objects.all()), 1)
+        self.response = self.client.get('/')
+        self.assertEqual(len(VisitorIp.objects.all()), 2)
